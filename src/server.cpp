b65ca46a90ab93163896b1cf4f3896fcd2e13f51
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2022-12-03 15:19:17 +0530

    updt: server: print local ip on init

--- src/server.cpp
+++ src/server.cpp
@@ -1,9 +1,13 @@
 #include "server.h"
+#include "displaymanager.h"
 #include <AsyncElegantOTA.h>
+
+#include <WiFi.h>
 
 AsyncWebServer ServerManager::webserver(80);
 
-bool alreadyInit = false;
+bool alreadyInit     = false;
+char messageBuff[50] = {0};
 
 void ServerManager::init() {
 	if(alreadyInit) {
@@ -15,5 +19,10 @@ void ServerManager::init() {
 
 	AsyncElegantOTA.begin(&webserver);
 	webserver.begin();
+
+	String s = WiFi.localIP().toString();
+	snprintf(messageBuff, 50, "Visit http://%s to configure..", s.c_str());
+	DisplayManager::printScrollingText(messageBuff);
+
 	alreadyInit = true;
 }
