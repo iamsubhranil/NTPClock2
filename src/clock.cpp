d334a30a7abef2cbfc3dce516b87d4997d778dd0
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2022-12-03 15:15:46 +0530

    updt: clock: changes in format

--- src/clock.cpp
+++ src/clock.cpp
@@ -2,7 +2,7 @@
 
 char Clock::timeStringBuff[50] = {0};
 
-const char *Clock::formatStrings[3] = {"%I:  %M %p", "%I   %M %p",
+const char *Clock::formatStrings[3] = {"%I : %M %p", "%I   %M %p",
                                        "%a,  %b %e"};
 
 int Clock::currentTick = 0;
