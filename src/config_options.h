f403a8094b8209a63ecc6e6619ca18df059c2d61
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2022-12-05 18:00:14 +0530

    updt: config: UI updates

--- src/config_options.h
+++ src/config_options.h
@@ -2,12 +2,12 @@
 #define OPTION(name, type, defaultValue, displayName)
 #endif
 
-OPTION(DisplayState, bool, true, "Display On")
 OPTION(APName, String, "ACAcessPoint", "Access Point Name")
 OPTION(APPassword, String, "RouteR1913", "Access Point Password")
 OPTION(NTPServer, String, "pool.ntp.org", "NTP Server")
 OPTION(OffsetHours, int, 5, "GMT Offset Hours")
 OPTION(OffsetMinutes, int, 30, "GMT Offset Minutes")
 OPTION(Username, String, "Subhranil", "Your name")
+OPTION(DisplayState, bool, true, "Display On")
 
 #undef OPTION
