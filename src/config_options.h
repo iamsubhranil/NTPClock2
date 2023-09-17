4a57c2ad40e704950cc294d48362571c3bf802a8
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2023-09-17 23:47:09 +0530

    updt: config: reorganized options

--- src/config_options.h
+++ src/config_options.h
@@ -8,11 +8,11 @@ OPTION(NTPServer, String, "pool.ntp.org", "NTP Server")
 OPTION(OffsetHours, int, 5, "GMT Offset Hours")
 OPTION(OffsetMinutes, int, 30, "GMT Offset Minutes")
 OPTION(Username, String, "Subhranil", "Your name")
+OPTION(Latitude, float, 22.5626, "Latitude")
+OPTION(Longitude, float, 88.363, "Longitude")
 OPTION(DisplayState, bool, true, "Display On")
 OPTION(TempFahrenhite, bool, false, "Temperature in °F")
 OPTION(Clock24, bool, false, "24 Hour Clock")
-OPTION(Latitude, float, 22.5626, "Latitude")
-OPTION(Longitude, float, 88.363, "Longitude")
 OPTION(ShowOutdoorTemperature, bool, true, "Show Outdoor Temperature")
 
 #undef OPTION
